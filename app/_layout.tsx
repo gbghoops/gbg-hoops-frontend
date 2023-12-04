82f199983b89f2b368c6929429bfe5d5fc84a512
Iyk Azorji <iykazorji@gmail.com>
2023-12-04 12:10:24 -0500

    Add linting and code formatting

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,41 +1,42 @@
-import { Slot } from "expo-router";
-import TamaguiConfig from "@/tamagui.config";
-import { TamaguiProvider, Theme } from "tamagui";
-import { useFonts } from "expo-font";
-import * as ScreenOrientation from "expo-screen-orientation";
-import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import { Slot } from "expo-router";
+import * as ScreenOrientation from "expo-screen-orientation";
+import * as SplashScreen from "expo-splash-screen";
+import { TamaguiProvider, Theme } from "tamagui";
+
+import TamaguiConfig from "@/tamagui.config";
 
 const MainLayout = () => {
-	SplashScreen.preventAutoHideAsync();
+    SplashScreen.preventAutoHideAsync();
 
-	async function changeScreenOrientation() {
-		await ScreenOrientation.lockAsync(
-			ScreenOrientation.OrientationLock.PORTRAIT_UP
-		);
-	}
-	const [loaded] = useFonts({
-		acumin_pro_bold: require("@assets/fonts/acumin-pro/acumin-pro-condensed-bold.otf"),
-		acumin_pro_semibold: require("@assets/fonts/acumin-pro/acumin-pro-condensed-semibold.otf"),
-		acumin_pro_regular: require("@assets/fonts/acumin-pro/acumin-pro-condensed.otf"),
-	});
+    async function changeScreenOrientation(): Promise<void> {
+        await ScreenOrientation.lockAsync(
+            ScreenOrientation.OrientationLock.PORTRAIT_UP,
+        );
+    }
+    const [loaded] = useFonts({
+        acumin_pro_bold: require("@assets/fonts/acumin-pro/acumin-pro-condensed-bold.otf"),
+        acumin_pro_semibold: require("@assets/fonts/acumin-pro/acumin-pro-condensed-semibold.otf"),
+        acumin_pro_regular: require("@assets/fonts/acumin-pro/acumin-pro-condensed.otf"),
+    });
 
-	useEffect(() => {
-		changeScreenOrientation();
-	}, []);
+    useEffect(() => {
+        changeScreenOrientation();
+    }, []);
 
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
-		}
-	}, [loaded]);
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
 
-	return (
-		<TamaguiProvider config={TamaguiConfig}>
-			<SafeAreaProvider>{loaded ? <Slot /> : null}</SafeAreaProvider>
-		</TamaguiProvider>
-	);
+    return (
+        <TamaguiProvider config={TamaguiConfig}>
+            <SafeAreaProvider>{loaded ? <Slot /> : null}</SafeAreaProvider>
+        </TamaguiProvider>
+    );
 };
 
 export default MainLayout;
--- babel.config.js
+++ babel.config.js
@@ -1,34 +1,34 @@
 module.exports = function (api) {
-	api.cache(true);
-	return {
-		presets: ["babel-preset-expo"],
-		plugins: [
-			// optional, only if you ever use process.env
-			"transform-inline-environment-variables",
-			"expo-router/babel",
-			// NOTE: this is optional, you don't *need* the compiler
-			// [
-			// 	"@tamagui/babel-plugin",
-			// 	{
-			// 		components: ["tamagui"],
-			// 		config: "./tamagui.config.ts",
-			// 		logTimings: true,
-			// 	},
-			// ],
-			[
-				"module-resolver",
-				{
-					root: ["./"],
-					extensions: [".ts", ".tsx"],
-					alias: {
-						"@assets": "./assets",
-						"@src": "./src",
-						"@": ["."],
-					},
-				},
-			],
-			// NOTE: this is only necessary if you are using reanimated for animations
-			"react-native-reanimated/plugin",
-		],
-	};
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+        plugins: [
+            // optional, only if you ever use process.env
+            "transform-inline-environment-variables",
+            "expo-router/babel",
+            // NOTE: this is optional, you don't *need* the compiler
+            // [
+            // 	"@tamagui/babel-plugin",
+            // 	{
+            // 		components: ["tamagui"],
+            // 		config: "./tamagui.config.ts",
+            // 		logTimings: true,
+            // 	},
+            // ],
+            [
+                "module-resolver",
+                {
+                    root: ["./"],
+                    extensions: [".ts", ".tsx"],
+                    alias: {
+                        "@assets": "./assets",
+                        "@src": "./src",
+                        "@": ["."],
+                    },
+                },
+            ],
+            // NOTE: this is only necessary if you are using reanimated for animations
+            "react-native-reanimated/plugin",
+        ],
+    };
 };
