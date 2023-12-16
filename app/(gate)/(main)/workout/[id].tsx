286ee66dab4c25a1606fea78609c9abad2f154c0
Iyk Azorji <iykazorji@gmail.com>
2023-12-16 15:12:40 -0500

    Update workout page to have ready screen

--- app/(gate)/(main)/workout/[id].tsx
+++ app/(gate)/(main)/workout/[id].tsx
@@ -1,16 +1,56 @@
+import { useEffect, useState } from "react";
+import { Dimensions } from "react-native";
 import { WorkoutHeader } from "@src/components/stack-header/WorkoutScreenHeader";
 import { Stack } from "expo-router";
 import { Text, View } from "tamagui";
 
 export default function WorkoutScreen() {
+    const [showReadyScreen, setShowReadyScreen] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setShowReadyScreen(false);
+        }, 3000);
+    }, []);
+
     return (
-        <View f={1} bg="$surface_background" jc="center" ai="center">
+        <View
+            f={1}
+            jc="center"
+            ai="center"
+            position="relative"
+            bg="$surface_background"
+        >
             <Stack.Screen
                 options={{
                     header: () => <WorkoutHeader />,
                 }}
             />
+            {showReadyScreen ? <ReadyScreen /> : null}
             <Text>Workout Screen</Text>
         </View>
     );
 }
+
+const ReadyScreen = () => {
+    const { width, height } = Dimensions.get("window");
+
+    return (
+        <View
+            f={1}
+            top={0}
+            left={0}
+            jc="center"
+            ai="center"
+            width={width}
+            height={"100%"}
+            position="absolute"
+            bg="$surface_background"
+            zIndex={1000}
+            borderWidth={1}
+            borderColor="red"
+        >
+            <Text>Ready Screen</Text>
+        </View>
+    );
+};
