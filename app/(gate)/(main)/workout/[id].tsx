ea7634d7c8c1a4dd469650b6dfe863fa1e00b269
Iyk Azorji <iykazorji@gmail.com>
2024-01-11 21:37:18 -0500

    Add confirm exit content

--- app/(gate)/(main)/workout/[id].tsx
+++ app/(gate)/(main)/workout/[id].tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import PagerView from "react-native-pager-view";
-import { FlashList } from "@shopify/flash-list";
 import DemoExerciseData, {
     Exercise,
     ExerciseData,
