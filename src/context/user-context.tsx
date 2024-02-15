1e79b11c9de40b548ca8beb66f175cb82ef546f8
Iyk Azorji <iykazorji@gmail.com>
2024-02-15 18:37:08 -0500

    add user context

---
+++ src/context/user-context.tsx
@@ -0,0 +1,65 @@
+import { createContext, PropsWithChildren, useContext } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchAuthSession } from "aws-amplify/auth";
+
+const fetchUser = async () => {
+    const backend_url = process.env.EXPO_PUBLIC_BACKEND_URL ?? "";
+
+    try {
+        const idToken = (await fetchAuthSession()).tokens?.idToken?.toString();
+        const response = await fetch(`${backend_url}/users/me`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${idToken}`,
+            },
+        });
+
+        console.log("Response: ", response);
+
+        if (!response.ok) {
+            throw new Error("Error fetching user data");
+        }
+
+        const data = await response.json();
+
+        const user = data;
+
+        return user;
+    } catch (error) {
+        throw new Error("Error fetching user data");
+    }
+};
+
+interface UserContextProps {
+    user: any;
+    error: any;
+    userDataLoading: boolean;
+}
+export const UserContext = createContext<UserContextProps | null>(null);
+
+export default function UserProvider({ children }: PropsWithChildren) {
+    const { data, isLoading, error } = useQuery({
+        queryKey: ["user"],
+        queryFn: fetchUser,
+    });
+
+    console.log("User Data: ", data);
+
+    return (
+        <UserContext.Provider
+            value={{ user: data?.user, userDataLoading: isLoading, error }}
+        >
+            {children}
+        </UserContext.Provider>
+    );
+}
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+
+    if (!context) {
+        throw new Error("useUser must be used within a UserProvider");
+    }
+
+    return context;
+};
