b2817d686a28b1813369eaf089e53c074f1db6a9
Iyk Azorji <iykazorji@gmail.com>
2024-02-20 13:10:04 -0500

    Add program and user context to authenticated route

--- app/(gate)/(main)/_layout.tsx
+++ app/(gate)/(main)/_layout.tsx
@@ -1,24 +1,30 @@
 import { Header } from "@src/components/stack-header/StackHeader";
+import ProgramsProvider from "@src/context/ProgramsContext/programs-context";
+import UserProvider from "@src/context/UserContext/user-context";
 import { colors } from "@src/styles/theme/colors";
 import { Stack } from "expo-router";
 
 export default function Layout() {
     return (
-        <Stack
-            screenOptions={{
-                header: () => null,
-                contentStyle: {
-                    backgroundColor: colors.surface_background,
-                },
-            }}
-        >
-            <Stack.Screen
-                name="settings"
-                options={{
-                    header: () => <Header />,
-                    presentation: "modal",
-                }}
-            />
-        </Stack>
+        <ProgramsProvider>
+            <UserProvider>
+                <Stack
+                    screenOptions={{
+                        header: () => null,
+                        contentStyle: {
+                            backgroundColor: colors.surface_background,
+                        },
+                    }}
+                >
+                    <Stack.Screen
+                        name="settings"
+                        options={{
+                            header: () => <Header />,
+                            presentation: "modal",
+                        }}
+                    />
+                </Stack>
+            </UserProvider>
+        </ProgramsProvider>
     );
 }
