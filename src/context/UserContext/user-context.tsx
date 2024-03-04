6d0b342d034f70b502e0f6c7c79bdff64d37d1cb
Iyk Azorji <iykazorji@gmail.com>
2024-03-04 18:11:57 -0500

    Remove logging user id:

--- src/context/UserContext/user-context.tsx
+++ src/context/UserContext/user-context.tsx
@@ -10,8 +10,6 @@ const fetchUser = async () => {
     try {
         const idToken = (await fetchAuthSession()).tokens?.idToken?.toString();
 
-        console.log("idToken", idToken);
-
         const response = await fetch(`${backend_url}/users/me`, {
             method: "GET",
             headers: {
